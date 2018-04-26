d0177c4ab9b0e9f8d634f826ac33d376f0b99a2e
Nazar Gerasymchuk <troyan3@gmail.com>
2018-04-26 17:59:32 +0300

    Refactored code

--- bot.py
+++ bot.py
@@ -1,6 +1,5 @@
 import logging
 import utils
-import random
 
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import Updater, CommandHandler, CallbackQueryHandler, MessageHandler, Filters
@@ -105,7 +104,7 @@ def prepareComicsToSend(comics):
     date = '{}/{}/{}'.format(comics['day'], comics['month'], comics['year'])
     link = '["{}" comics on XKCD](https://xkcd.com/{})'.format(comics['title'], comics['num'])
     text2send = '*{}. {}*. {}\n\n_{}_, {}'.format(comics['num'], comics['title'], comics['alt'], date, link)
-    return [text2send, comics['img'], comics['num']]
+    return [text2send, comics['img']]
 
 # Inline keyboard for comics with respective callbacks. Instantiated once.
 comics_keyboard = InlineKeyboardMarkup([
@@ -134,7 +133,44 @@ def sendComics(bot, cur_chat_id, prepared_comics):
                      disable_web_page_preview=True, text=prepared_comics[0], reply_markup=comics_keyboard)
     # logger.info("sent: {} -- {}".format(prepared_comics[0], prepared_comics[1]))
 
+def getRequestedComics(update, request):
+    comics = ''
+    if request == states.S_NEWEST:
+        comics = Comics.getLatestComics()
+    elif request == states.S_OLDEST:
+        comics = Comics.getOldestComics()
+    elif request == states.S_RANDOM:
+        comics = Comics.getRandomComics()
+    elif request == states.S_PREV:
+        currentNum = getUserComicsNumber(update)
+        if currentNum == Checker.UNDEFINED:
+            newNum = Checker.LatestComicsNumber - 1
+        newNum = currentNum - 1
+        if Comics.isComicsAvailable(newNum):
+            comics = Comics.getComicsByNumber(newNum)
+        else:
+            user_friendly_warning = "There is no comics with #{}. If you think it is problem -- write to authors. \n" \
+                                    "Just in case, XKCD updates every Monday, Wednesday, and Friday.".format(currentNum)
+            raise ValueError('No requested comics.',  user_friendly_warning)
+    elif request == states.S_NEXT:
+        currentNum = getUserComicsNumber(update)
+        newNum = currentNum + 1
+        if Comics.isComicsAvailable(newNum):
+            comics = Comics.getComicsByNumber(newNum)
+        else:
+            user_friendly_warning = "Unfortunately we can't get your {} comics. Probably it will be ready soon. \n"  \
+                                    "XKCD updates every Monday, Wednesday, and Friday.".format(currentNum)
+            raise ValueError('No requested comics (yet).',  user_friendly_warning)
+    return comics
+
+def sendMessaageAboutError(bot, cur_chat_id, errText):
+    """ Sends error message with funny picture. """
+    bot.send_photo(chat_id=cur_chat_id, disable_notification=True, photo=open(QuestionImage, 'rb'))
+    bot.send_message(chat_id=cur_chat_id, disable_notification=False, parse_mode='Markdown',
+                     disable_web_page_preview=True, text=errText)
+
 def onButtonClicked(bot, update):
+    comics = ''
     query = update.callback_query
     cur_chat_id = query.message.chat_id
     if query.data:
@@ -146,60 +182,24 @@ def onButtonClicked(bot, update):
     user_state = getUserState(update)
     # TODO: Use user_state later, when there will be search
 
-    # if user_state == states.S_START:
-    if query.data == states.S_NEWEST:
-        prepared_comics = prepareComicsToSend(Comics.getLatestComics())
-        sendComics(bot, cur_chat_id, prepared_comics)
-        setUsersComicsNumber(update, prepared_comics[2])
-        setUserState(update, states.S_START)
-
-    # if user_state == states.S_START:
-    if query.data == states.S_OLDEST:
-        prepared_comics = prepareComicsToSend(Comics.getComicsByNumber(1))
-        sendComics(bot, cur_chat_id, prepared_comics)
-        setUsersComicsNumber(update, prepared_comics[2])
-        setUserState(update, states.S_START)
-
     if query.data == states.S_NUMBER:
         text2send = "Please, enter integer number of XKCD comics (first one is `1` and last one is `{}`)"\
                     .format(Checker.LatestComicsNumber)
-        bot.send_message(chat_id=cur_chat_id,
-                         disable_notification=False,
-                         parse_mode='Markdown',
-                         text=text2send)
+        bot.send_message(chat_id=cur_chat_id, disable_notification=False, parse_mode='Markdown', text=text2send)
         setUserState(update, states.S_NUMBER)
+    else:
+        try:
+            comics = getRequestedComics(update, query.data)
+        except ValueError as err:
+            sendMessaageAboutError(bot, cur_chat_id, err.args[0])
 
-    if query.data == states.S_RANDOM:
-        random_number = random.randint(1, Checker.LatestComicsNumber + 1)
-        prepared_comics = prepareComicsToSend(Comics.getComicsByNumber(random_number))
-        sendComics(bot, cur_chat_id, prepared_comics)
-        setUsersComicsNumber(update, prepared_comics[2])
-        setUserState(update, states.S_START)
-
-    if query.data == states.S_PREV:
-        currentNum = getUserComicsNumber(update)
-        if currentNum == Checker.UNDEFINED:
-            newNum = Checker.LatestComicsNumber - 1
-        newNum = currentNum - 1
-        if Comics.isComicsAvailable(newNum):
-            prepared_comics = prepareComicsToSend(Comics.getComicsByNumber(newNum))
-            sendComics(bot, cur_chat_id, prepared_comics)
-            setUsersComicsNumber(update, prepared_comics[2])
-            setUserState(update, states.S_START)
-
-    if query.data == states.S_NEXT:
-        currentNum = getUserComicsNumber(update)
-
-        newNum = currentNum + 1
-        if Comics.isComicsAvailable(newNum):
-            prepared_comics = prepareComicsToSend(Comics.getComicsByNumber(newNum))
-            sendComics(bot, cur_chat_id, prepared_comics)
-            setUsersComicsNumber(update, prepared_comics[2])
-        else:
-            prepared_comics = [QuestionImage,
-                               "Unfortunately we can't get your {} comics. Probably it will be ready soon. "
-                               "XKCD updates every Monday, Wednesday, and Friday.".format(currentNum)]
-            sendComics(bot, cur_chat_id, prepared_comics)
+        if comics != '':
+            if 'num' in comics:
+                num = comics['num']
+                prepared_comics = prepareComicsToSend(comics)
+                sendComics(bot, cur_chat_id, prepared_comics)
+                setUsersComicsNumber(update, num)
+                setUserState(update, states.S_START)
 
     setUserState(update, states.S_START)
 
@@ -215,27 +215,29 @@ def onMessage(bot, update):
                 setUserState(update, states.S_START)
                 return
             else:
-                sendComics(bot, update.effective_chat.id,
-                           ["Unfortunately there is no comics with number `{}` :( \n"
-                            "Let's hope that someday there will be even `{}` comics on XKCD! \n\n"                           
-                            "But still you can use /menu for another requests.".format(num, num + 1), QuestionImage])
+                sendMessaageAboutError(bot, update.effective_chat.id, )
+                user_friendly_text = "Unfortunately there is no comics with number `{}` :( \n" \
+                            "Let's hope that someday there will be even `{}` comics on XKCD! \n\n" \
+                            "But still you can use /menu for another requests.".format(num, num + 1)
+                sendMessaageAboutError(bot, update.effective_chat.id, user_friendly_text)
                 setUserState(update, states.S_START)
                 return
 
         else:
             setUserState(update, states.S_NUMBER)
-            sendComics(bot, update.effective_chat.id,
-                       ["`{}` is not a valid integer number, you know it. \n\n"
-                        "Please, send me *only* number, without any other symbols. "
-                        "Examples of correct numbers: `13`, `42`, `666`, `1000`, ...\n\n"
-                        "Or you can use /menu as usual.".format(msg), QuestionImage])
+            user_friendly_text = "`{}` is not a valid integer number, you know it. \n\n" \
+                        "Please, send me *only* number, without any other symbols. " \
+                        "Examples of correct numbers: `13`, `42`, `666`, `1000`, ...\n\n" \
+                        "Or you can use /menu as usual.".format(msg)
+
+            sendMessaageAboutError(bot, update.effective_chat.id, user_friendly_text)
             return
 
     # Problem happened
-    sendComics(bot, update.effective_chat.id,
-               ["We tried to understand your answer, but we forgot what was the question... "
-                "Hope our admins will check logs and solve this problem in newer versions of this bot. \n\n"
-                "Meanwhile you can use /menu for another requests.", QuestionImage])
+    user_friendly_text = "We tried to understand your answer, but we forgot what was the question... " \
+                "Hope our admins will check logs and solve this problem in newer versions of this bot. \n\n" \
+                "Meanwhile you can use /menu for another requests."
+    sendMessaageAboutError(bot, update.effective_chat.id, user_friendly_text)
     setUserState(update, states.S_START)
 
 
